9291f298283aa49014900cff3791d91fdd6ccfc1
hyojin202406 <hyojin202406@gmail.com>
2024-10-06 16:57:15 +0900

    first commit

---
+++ src/test/java/com/hhplu/hhplusconcert/HhplusConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplu.hhplusconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConcertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
