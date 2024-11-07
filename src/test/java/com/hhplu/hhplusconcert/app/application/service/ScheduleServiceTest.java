5c3ba9932bfe6a311aa5cf5da0d33ac24c956d21
hyojin202406 <hyojin202406@gmail.com>
2024-11-07 15:32:31 +0900

    test: 레디스 캐싱 검증 테스트 추가

--- src/test/java/com/hhplu/hhplusconcert/app/application/service/ScheduleServiceTest.java
+++ src/test/java/com/hhplu/hhplusconcert/app/application/service/ScheduleServiceTest.java
@@ -2,20 +2,59 @@ package com.hhplu.hhplusconcert.app.application.service;
 
 import com.hhplu.hhplusconcert.app.application.service.concert.service.ScheduleService;
 import com.hhplu.hhplusconcert.app.domain.concert.entity.Schedule;
+import com.hhplu.hhplusconcert.app.domain.concert.repository.ScheduleRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Arrays;
 import java.util.List;
 
-@SpringBootTest(properties = "spring.config.location=classpath:/application-mysql.yml")
-@ActiveProfiles("mysql")  // 필요에 따라 특정 프로파일을 활성화
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+//@SpringBootTest(properties = "spring.config.location=classpath:/application-mysql.yml")
+//@ActiveProfiles("mysql")  // 필요에 따라 특정 프로파일을 활성화
+@SpringBootTest
 class ScheduleServiceTest {
 
     @Autowired
     private ScheduleService scheduleService;
+
+    @MockBean
+    private ScheduleRepository scheduleRepository;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @BeforeEach
+    void saveCache() {
+        Long concertId = 1L;
+        List<Schedule> mockSchedules = Arrays.asList(
+                new Schedule(1L, concertId, null, null),
+                new Schedule(2L, concertId, null, null)
+        );
+
+        // 스케줄 조회 시 mockSchedules 반환
+        when(scheduleRepository.existsSchedule(concertId)).thenReturn(mockSchedules);
+
+        // When: 첫 번째 호출 -> DB에서 데이터 조회
+        scheduleService.redisCacheableSchedule(concertId);
+    }
+
+    @AfterEach
+    void clearCache() {
+        // 특정 캐시 키 삭제
+        redisTemplate.delete("concertSchedules::1");
+    }
+
 
     @Test
     void 스케줄_조회_성공() {
@@ -30,7 +69,7 @@ class ScheduleServiceTest {
 
         System.out.println("스케줄 조회 경과 시간 (나노초): " + duration);
 
-        Assertions.assertThat(schedules).hasSize(2);
+        assertThat(schedules).hasSize(2);
     }
 
     @Test
@@ -48,6 +87,20 @@ class ScheduleServiceTest {
 
         System.out.println("레디스 캐싱 스케줄 조회 경과 시간 (나노초): " + duration);
 
-        Assertions.assertThat(schedules).hasSize(2);
+        assertThat(schedules).hasSize(2);
     }
+
+    @Test
+    void 레디스_캐싱_스케줄_조회_검증_성공() {
+        // Given
+        Long concertId = 1L;
+
+        // When: 두 번째 호출 -> 캐시에서 데이터 반환
+        List<Schedule> schedules = scheduleService.redisCacheableSchedule(concertId);
+
+        // Then: 두 번째 호출의 결과를 검증
+        assertThat(schedules).hasSize(2);
+        verify(scheduleRepository, times(0)).existsSchedule(concertId);
+    }
+
 }
