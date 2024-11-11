5bd26b51e27aaae3fc932166a9a9b3839edd2608
hyojin202406 <hyojin202406@gmail.com>
2024-11-11 17:08:40 +0900

    fix: 대기열 코드 일관성 및 테스트 용이성 반영

--- src/main/java/com/hhplu/hhplusconcert/app/application/scheduler/WaitingQueueStatusScheduler.java
+++ src/main/java/com/hhplu/hhplusconcert/app/application/scheduler/WaitingQueueStatusScheduler.java
@@ -1,43 +1,33 @@
 package com.hhplu.hhplusconcert.app.application.scheduler;
 
+import com.hhplu.hhplusconcert.app.application.service.waitingqueue.service.WaitingQueueRedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class WaitingQueueStatusScheduler {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    private static final String WAITING_QUEUE_KEY = "waitingUserQueue"; // Sorted Set 키 (우선순위 대기열)
-    private static final String ACTIVE_QUEUE_KEY = "activeUserQueue"; // 활성 사용자 대기열 (List)
+    private final WaitingQueueRedisService waitingQueueRedisService;
 
     // 스케줄러로 활성 유저 대기열에 10명씩 추가
-    @Scheduled(cron = "0 0/1 * * * *")
+    @Scheduled(cron = "0 0/10 * * * *")
     public void activateUsers() {
-        log.info("스케줄러 시작");
-        Set<Object> tokens = redisTemplate.opsForZSet().range(WAITING_QUEUE_KEY, 0, 9);
-        log.info("tokens : {}", tokens);
-        if (tokens != null && !tokens.isEmpty()) {
-            for (Object token : tokens) {
-                // 활성 사용자 대기열에 추가
-                redisTemplate.opsForList().rightPush(ACTIVE_QUEUE_KEY, token);
-                // 대기열에서 제거
-                redisTemplate.opsForZSet().remove(WAITING_QUEUE_KEY, token);
+        for (int i = 0; i < 10; i++) {
+            ZSetOperations.TypedTuple<Object> tokenWithScore = waitingQueueRedisService.popMinFromWaitingQueue();
+            if (tokenWithScore != null) {
+                Object token = tokenWithScore.getValue();
+                waitingQueueRedisService.moveToActiveQueue(token);
+            } else {
+                break; // 더 이상 대기열에 사용자가 없으면 중단
             }
         }
 
-        // TTL 설정: 3분, 한번만 설정하기 위해 조건 추가
-        if (redisTemplate.getExpire(ACTIVE_QUEUE_KEY) == -1) {
-            redisTemplate.expire(ACTIVE_QUEUE_KEY, 180, TimeUnit.SECONDS);
-        }
-
+        waitingQueueRedisService.setActiveQueueTTL();
     }
+
 }
