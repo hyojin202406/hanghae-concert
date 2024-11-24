1fe34266c7edc739c7b8ab67c54c1060f4e41921
hyojin202406 <hyojin202406@gmail.com>
2024-11-24 20:24:37 +0900

    feat: Message 객체에 messageKey, version, created_at 등의 정보 추가

--- src/main/java/com/hhplu/hhplusconcert/app/application/event/payment/PaymentSuccessEvent.java
+++ src/main/java/com/hhplu/hhplusconcert/app/application/event/payment/PaymentSuccessEvent.java
@@ -2,17 +2,22 @@ package com.hhplu.hhplusconcert.app.application.event.payment;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 public class PaymentSuccessEvent {
-    private final String eventKey;
-    private final Long orderKey;
-    private final Long paymentKey;
+    private final String eventKey;          // 고유 식별자
+    private final Long orderKey;            // 주문 키
+    private final Long paymentKey;          // 결제 키
+    private final String version;           // 메시지 버전
+    private final LocalDateTime createdAt;  // 생성 시간
 
     public PaymentSuccessEvent(Long orderKey, Long paymentKey) {
-        this.eventKey = UUID.randomUUID().toString(); // 동일한 UUID 생성
+        this.eventKey = UUID.randomUUID().toString();
         this.orderKey = orderKey;
         this.paymentKey = paymentKey;
+        this.version = "1.0";
+        this.createdAt = LocalDateTime.now();
     }
 }
--- src/main/java/com/hhplu/hhplusconcert/app/application/event/reservation/ReservationSuccessEvent.java
+++ src/main/java/com/hhplu/hhplusconcert/app/application/event/reservation/ReservationSuccessEvent.java
@@ -2,17 +2,22 @@ package com.hhplu.hhplusconcert.app.application.event.reservation;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 public class ReservationSuccessEvent {
-    private final String eventKey;
-    private final Long reservationId;
-    private final Long reservationKey;
+    private final String eventKey;          // 고유 식별자
+    private final Long reservationId;       // 예약 아이디
+    private final Long reservationKey;      // 예약 키
+    private final String version;           // 메시지 버전
+    private final LocalDateTime createdAt;  // 생성 시간
 
     public ReservationSuccessEvent(Long reservationId, Long reservationKey) {
         this.eventKey = UUID.randomUUID().toString(); // 동일한 UUID 생성
         this.reservationId = reservationId;
         this.reservationKey = reservationKey;
+        this.version = "1.0";
+        this.createdAt = LocalDateTime.now();
     }
 }
