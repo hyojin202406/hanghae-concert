4493c8f63c371f3b593d7c1b573c33c1a20b4f33
hyojin202406 <hyojin202406@gmail.com>
2024-11-24 20:17:38 +0900

    refactor: Outbox 정적 팩토리 메서드 패턴 적용

--- src/main/java/com/hhplu/hhplusconcert/app/domain/outbox/entity/Outbox.java
+++ src/main/java/com/hhplu/hhplusconcert/app/domain/outbox/entity/Outbox.java
@@ -1,6 +1,8 @@
 package com.hhplu.hhplusconcert.app.domain.outbox.entity;
 
 import com.hhplu.hhplusconcert.app.domain.outbox.OutboxStatus;
+import com.hhplu.hhplusconcert.common.error.ErrorCode;
+import com.hhplu.hhplusconcert.common.exception.BaseException;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,6 +37,22 @@ public class Outbox {
 
     @Column(nullable = false, name = "created_at")
     private LocalDateTime createdAt;
+
+    public static Outbox createOutbox(String type, String eventKey, String payload) {
+        if (!type.equals("payment") && !type.equals("reservation")) {
+            throw new BaseException(ErrorCode.COMMON_BAD_REQUEST);
+        }
+
+        Outbox outbox = Outbox.builder()
+                .eventKey(eventKey)
+                .eventType(type)
+                .payload(payload)
+                .outboxStatus(OutboxStatus.INIT)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        return outbox;
+    }
 
     public void publishedStaus() {
         this.outboxStatus = OutboxStatus.PUBLISHED;
