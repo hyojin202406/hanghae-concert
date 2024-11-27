f14fae1200b777f8c583d2c7e198fc6dbb734329
hyojin202406 <hyojin202406@gmail.com>
2024-11-27 10:35:50 +0900

    chore/feat: 대기열 토큰 스케줄러 예외처리 추가, 레디스 설정 변경

--- src/main/java/com/hhplu/hhplusconcert/common/config/JacksonConfig.java
+++ src/main/java/com/hhplu/hhplusconcert/common/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.hhplu.hhplusconcert.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,8 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        // 날짜 및 시간 직렬화를 ISO-8601 형식으로 설정
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
 }
